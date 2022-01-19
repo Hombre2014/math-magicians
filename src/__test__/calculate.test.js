5aebc5f7c6fa3db6b647d8941552e78be899bf88
kingqabil <kingqabil@live.com>
2022-01-19 17:46:05 +0300

    Added calculate test function

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -18,4 +18,19 @@ describe('Test calculate function', () => {
     fireEvent.click(equals);
     expect(display.innerHTML).toBe('3');
   });
+
+  it('Checking for complete operation', () => {
+    render(<Calculator />);
+    const three = screen.getByText('3');
+    const sub = screen.getByText('-');
+    const six = screen.getByText('6');
+    const equals = screen.getByText('=');
+    const display = screen.getByTestId('display');
+    fireEvent.click(three);
+    fireEvent.click(sub);
+    fireEvent.click(six);
+    fireEvent.click(equals);
+    expect(display.innerHTML).toBe('-3');
+  });
+
 });
