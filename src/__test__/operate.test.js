936fe1585a45b4efe78b66f7900eeaece41501db
kingqabil <kingqabil@live.com>
2022-01-19 17:10:49 +0300

    Added Sub&Div test

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -8,4 +8,12 @@ describe('Check for the 4 operation +. -. / and *', () => {
   test('For subtracting two numbers', () => {
     expect(operate(4, 3, '-')).toEqual('1');
   });
+
+  test('For Multiplication two numbers', () => {
+    expect(operate(4, 3, 'x')).toEqual('12');
+  });
+
+  test('For Divition two numbers', () => {
+    expect(operate(4, 2, '÷')).toEqual('2');
+  });
 });
--- src/__test__/pages.test.js
+++ src/__test__/pages.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Home from '../components/Home';
+import Quote from '../components/Quote';
 
 describe('Testing page render', () => {
   it('Home page renders correctly', () => {
     const { asFragment } = render(<Home />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('Quote page renders correctly', () => {
+    const { asFragment } = render(<Quote />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
