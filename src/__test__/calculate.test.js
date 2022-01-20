faf9b881b7cedd1d18759a10a9e896f6dbc231d8
kingqabil <kingqabil@live.com>
2022-01-20 12:08:21 +0300

    Added request changes

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -3,8 +3,14 @@ import {
   render, screen, fireEvent,
 } from '@testing-library/react';
 import Calculator from '../components/Calculator';
+import calculate from '../logic/calculate';
 
 describe('Test calculate function', () => {
+  const obj = {
+    total: null,
+    next: null,
+    operation: null,
+  };
   it('Checking for complete operation', () => {
     render(<Calculator />);
     const one = screen.getByText('1');
@@ -37,4 +43,29 @@ describe('Test calculate function', () => {
     const { asFragment } = render(<Calculator />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  obj.next = '1';
+  test('Pressing button +/- and next is 1 it will returns -1', () => {
+    expect(calculate(obj, '+/-')).toEqual({
+      total: null,
+      next: '-1',
+      operation: null,
+    });
+  });
+
+  test('When next is 5 and press the . button, it will returns 5.', () => {
+    const testOperation = { total: null, next: '5', operation: '.' };
+    expect(calculate(testOperation, '.')).toEqual({ total: null, next: '5.', operation: '.' });
+  });
+
+  test('User pressed an operation + after pressing = button', () => {
+    const testOperation = { total: '10', next: null, operation: '+' };
+    expect(calculate(testOperation, '+')).toEqual({ total: '10', next: null, operation: '+' });
+  });
+
+  test('User pressed an operation + button and there is an existing operation x', () => {
+    const testOperation = { total: '10', next: null, operation: 'x' };
+    expect(calculate(testOperation, '+')).toEqual({ total: '10', next: null, operation: '+' });
+  });
+
 });
